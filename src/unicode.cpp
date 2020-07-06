90520f807f824a8b40bf00316ad618cda209bc71
Altair Wei <altair_wei@outlook.com>
2020-07-06 22:37:31 +0800

    add unicode test

---
+++ src/unicode.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+template <typename T>
+void dump(const T& str)
+{
+  for (char ch : str) {
+    printf(
+      "%.2x ",
+      static_cast<unsigned char>(ch));
+  }
+  putchar('\n');
+}
+
+int main()
+{
+  char str[] = "我";
+  char u8str[] = u8"我";
+  dump(str);
+  dump(u8str);
+}
